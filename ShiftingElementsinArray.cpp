37dceb050881df1a5c37713679189a1740c8154b
Krishnesh chaurasia <krishneshchaurasia1@gmail.com>
2022-08-09 18:20:04 +0530

    Update : Errors Resolved.

--- ShiftingElementsinArray.cpp
+++ ShiftingElementsinArray.cpp
@@ -13,12 +13,13 @@ class Array{
     };
     
 void Array::insert(int loc, int element){
+    int i;
     if(maxsize >= 100 && loc >= maxsize){
         cout<<"Insertion not possible..!";
         exit(0);
     }
     else{
-        for(int i = maxsize - 1; i >= loc; i--){
+        for(i = maxsize - 1; i >= loc; i--){
             x[i+1] = x[i];
         }
         x[i] = element;
@@ -30,10 +31,8 @@ int Array::search(int k){
         if(x[i] == k){
             return i;
         }
-        else{
-            return 0;
-        }
     }
+    return -1;
 }
 
 Array::Array(){
@@ -46,7 +45,7 @@ void Array::Read_Element(){
         cout<<"Array maxed out!";
     }
     else{
-        cout<<"Enter"<<maxsize<<" number into the array";
+        cout<<"Enter "<<maxsize<<" number into the array : ";
         for(int i = 0; i < maxsize; i++){
             cin>>x[i];
         }
@@ -55,7 +54,7 @@ void Array::Read_Element(){
 
 void Array::Display_Element(){
     for(int i = 0; i < maxsize; i++){
-        cout<<"the element of the array";
+        cout<<"The elements of the array are : ";
         cout<<x[i]<<endl;
     }
 }
@@ -68,17 +67,20 @@ void Array::Display_Element_Backward(){
 
 int main()
 {
+    int key,location,element;
+    
     Array arr1;
     arr1.Read_Element();
     arr1.Display_Element();
-    cout<<"Enter the element for searching :";
+    cout<<"Enter the element to be searched : ";
     cin>>key;
-    r = arr1.search(key);
-    cout<<"The element found at"<<r<<" .";
-    cout<<"Insert the location of the element :";
+    int r = arr1.search(key);
+    cout<<"The element found at index "<<r<<" ."<<endl;
+    cout<<"Insert the location of the element : ";
     cin>>location;
-    cout<<"Enter the element you want to insert :";
+    cout<<"Enter the element you want to insert : ";
     cin>>element;
-    r = arr1.insert(location,element);
+    arr1.insert(location,element);
+    arr1.Display_Element();
     return 0;
 }
