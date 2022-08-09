433a58c9d7710dc8024eef462de62b9ab5733e56
Krishnesh chaurasia <krishneshchaurasia1@gmail.com>
2022-08-09 10:02:16 +0530

    Linear search base file

---
+++ LinearSearch.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+class Array{
+    public:
+        int x[100];
+        int maxsize;
+        Array();
+        void Read_Element();
+        void Display_Element();
+        void Display_Element_Backward();
+        int search(int);
+    };
+    
+int Array::search(int k){
+    for(int i = 0; i < maxsize; i++){
+        if(x[i] == k){
+            return i;
+        }
+        else{
+            return 0;
+        }
+    }
+}
+
+Array::Array(){
+    cout<<"Enter the size of the array : ";
+    cin>>maxsize;
+}
+
+void Array::Read_Element(){
+    if(maxsize > 100){
+        cout<<"Array maxed out!";
+    }
+    else{
+        cout<<"Enter"<<maxsize<<" number into the array";
+        for(int i = 0; i < maxsize; i++){
+            cin>>x[i];
+        }
+    }
+}
+
+void Array::Display_Element(){
+    for(int i = 0; i < maxsize; i++){
+        cout<<"the element of the array";
+        cout<<x[i]<<endl;
+    }
+}
+
+void Array::Display_Element_Backward(){
+    for(int i = maxsize - 1; i >= 0; i--){
+        cout<<x[i];
+    }
+}
+
+int main()
+{
+    Array arr1;
+    arr1.Read_Element();
+    arr1.Display_Element();
+    return 0;
+}
